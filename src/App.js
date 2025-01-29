f33e23b38cf5d4a8e0c07bfa7e605a660ccac82c
alibadawi25 <alibadawi25@gmail.com>
2025-01-29 15:13:16 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -8,10 +8,44 @@ export default function DownloadPage() {
 
   return (
     <div style={{ textAlign: "center", padding: "50px" }}>
+      {/* Banner Image */}
+      <img
+        src="https://i.ibb.co/zhQn8QGq/Code-for-life.png" // Replace with your own image
+        alt="Banner"
+        style={{
+          width: "80%",
+          maxWidth: "800px",
+          borderRadius: "10px",
+          marginBottom: "20px",
+        }}
+      />
+
       <h1>Download Code For Life</h1>
-      <Button type="primary" size="large" onClick={downloadExe}>
-        Download Now
-      </Button>
+
+      {/* Description */}
+      <p style={{ fontSize: "18px", color: "#555", marginBottom: "20px" }}>
+        "Code For Life" is an exciting game that makes you learn how to code in
+        a fun way!!
+      </p>
+
+      {/* Container for button and secondary image */}
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        {/* Secondary Image Next to Button */}
+        <img
+          src="https://i.ibb.co/bRJ53BZS/Code-For-life-1.png" // Replace with your own image
+          alt="Download Icon"
+          style={{ width: "80px", marginRight: "10px" }} // Adjust size and margin
+        />
+        <Button type="primary" size="large" onClick={downloadExe}>
+          Download Now
+        </Button>
+      </div>
     </div>
   );
 }
