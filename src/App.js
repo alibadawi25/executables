8451c05e3a1535d6c149f6ade5ab87e86bca8ecb
alibadawi25 <alibadawi25@gmail.com>
2025-01-29 14:59:25 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-import logo from "./logo.svg";
-import "./App.css";
+import { Button } from "antd";
 
-function App() {
-  return <div className="App"></div>;
+export default function DownloadPage() {
+  const downloadExe = () => {
+    window.location.href =
+      "https://github.com/alibadawi25/executables/releases/download/v0.0.0/Code.For.Life.exe";
+  };
+
+  return (
+    <div style={{ textAlign: "center", padding: "50px" }}>
+      <h1>Download Code For Life</h1>
+      <Button type="primary" size="large" onClick={downloadExe}>
+        Download Now
+      </Button>
+    </div>
+  );
 }
-
-export default App;
